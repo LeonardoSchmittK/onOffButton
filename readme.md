0e041bcceb0cb77cf6a1624d8d31235877b761d8
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-09-29 21:07:20 -0300

    adding screenshot and finishing the entire project...

--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 # Button On and Off effect
+
+![screenshot-project](screenshot.png)
+
++ Css
++ Html 
++ JavaScript 
+
+- [x] Make a responsive and neat switch.
+- [x] Learn something
+
+
+*Made With :purple_heart:*
--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-let btn = window.document.querySelector('#button-toggle');
-let circle = window.document.querySelector('.circle');
+let btn = window.document.querySelector("#button-toggle");
+let circleOff = window.document.querySelector(".circle-off");
+let circleOn = window.document.querySelector(".circle-on");
+let txt = window.document.getElementsByClassName("txt");
+
+function toggleEffect() {
+   btn.style.filter = "drop-shadow(1px 1px 10px grey)";
+ }
+
+function clearShadowFilter() {
+   btn.style.filter = "none";
+ }
+
 function putOn() {
-   circle.style.marginLeft = '70px';
-   
-   circle.onclick = () =>{
-   circle.style.marginLeft = '0px';    
-   }
-   
+  circleOff.style.marginLeft = "60px";
+  circleOn.style.display = "block";
+  txt[1].style.display = "block";
+  txt[0].style.display = "none";
+
+  setTimeout(toggleEffect, 10);
+  setTimeout(clearShadowFilter, 10000);
 }
 
+function putOff() {
+  circleOff.style.marginLeft = "10px";
+  circleOn.style.display = "none";
+  txt[1].style.display = "none";
+  txt[0].style.display = "block";
+ 
+  setTimeout(toggleEffect, 10);
+  setTimeout(clearShadowFilter, 10000);
+}
